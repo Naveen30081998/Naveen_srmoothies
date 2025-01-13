1be95fedcf19960e2862868a1ddb20eb0b31f5f8
Naveen30081998 <kns9182488956@gmail.com>
2025-01-13 10:19:24 +0000

    Update streamlit_app.py
    table code added

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ if ingredients_list:
             values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
    #st.write(my_insert_stmt)
-   #st.stop()
+   #st.stop()t
    time_to_insert = st.button('Submit Order')
 
    if time_to_insert:
@@ -47,4 +47,5 @@ if ingredients_list:
 #NEW SECTION TO DISPLAY SMOOTHIEFRUIT TO DISPLAY INFORMATION
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
